8190047c772c750929b498f8576b5596a8dc5e70
milesst <79859505+milesst@users.noreply.github.com>
2023-06-15 23:17:11 +0300

    db data

--- src/config/base-config.js
+++ src/config/base-config.js
@@ -1,8 +1,8 @@
 let config = {
     "db": {
-        "host": "localhost",
-        "user": "postgres",     
-        "password": "123",
+        "host": "dpg-ci5mdrtgkuvgpfcr0qo0-a",
+        "user": "admin",     
+        "password": "Xmx1goaz757TM8bvwVsTaekwlkQZGRiP",
         "database": "voice_journal_db",
         "port": 5432,
         "ssl": false
